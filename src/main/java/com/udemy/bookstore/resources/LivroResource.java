bf48852fdbadd8d8b258b094111d7174f8ea6c9f
FlvAvsGit <flvavsgit@gmail.com>
2021-06-16 19:49:37 -0300

    Put e Patch para Livros

--- src/main/java/com/udemy/bookstore/resources/LivroResource.java
+++ src/main/java/com/udemy/bookstore/resources/LivroResource.java
@@ -1,15 +1,21 @@
 package com.udemy.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udemy.bookstore.domain.Livro;
 import com.udemy.bookstore.dto.LivroDTO;
@@ -23,29 +29,51 @@ public class LivroResource {
 	private LivroService livserv;
 
 //	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	@GetMapping(value="/{id}")
-	public ResponseEntity<?> find (@PathVariable Integer id) {
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 //		System.out.println("Entrei no /id");
 		Livro objRespEnt = livserv.metodoFindService(id);
 		return ResponseEntity.ok().body(objRespEnt);
 	}
+
 //	@RequestMapping(value = "/tudo", method = RequestMethod.GET)
-	@GetMapping(value="/")
+	@GetMapping(value = "/")
 	public ResponseEntity<?> findAll() {
 		List<Livro> objRespEnt = livserv.metodoFindAllService();
 		return ResponseEntity.ok().body(objRespEnt);
 	}
 
-	// localhost:8080/livros?categoria=1  			Todos os livros (Somente Id e Titulo) da Categoria 1
-	
+	// localhost:8080/livros?categoria=1 Todos os livros (Somente Id e Titulo) da
+	// Categoria 1
+
 	@GetMapping
-	public ResponseEntity<List<LivroDTO>> findLivroDto(@RequestParam(value = "catigoria", defaultValue = "0") Integer id_cat) {
+	public ResponseEntity<List<LivroDTO>> findLivroDto(
+			@RequestParam(value = "catigoria", defaultValue = "0") Integer id_cat) {
 		List<Livro> listLivro = livserv.livrosPorCat(id_cat);
 //		List<CategoriaDTO> categoriaDtoList = categoriaList.stream().map(obj -> new CategoriaDTO(obj))
 //				.collect(Collectors.toList());
 		List<LivroDTO> livroDtoList = listLivro.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(livroDtoList);
 	}
-		
-}
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro livroObj) {
+		Livro newLivroObj = livserv.update(id, livroObj);
+		return ResponseEntity.ok().body(newLivroObj);
+	}
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro livroObj) {
+		Livro newLivroObj = livserv.update(id, livroObj);
+		return ResponseEntity.ok().body(newLivroObj);
+	}
+
+	
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestBody Livro livroObj) {
+		livroObj = livserv.create(livroObj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livroObj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+}
--- src/main/java/com/udemy/bookstore/service/LivroService.java
+++ src/main/java/com/udemy/bookstore/service/LivroService.java
@@ -34,10 +34,28 @@ public class LivroService {
 	public List<Livro> livrosPorCat(Integer id_cat) {
 		// Ver se a Categoria existe
 		categoriaRepositorio.findById(id_cat);
-			
 		return livroRepositorio.findLivrosByCategoria(id_cat);
 	}
 
+	public Livro create(Livro livroObj) {
+		livroObj.setId(null);
+		return livroRepositorio.save(livroObj);
+
+	}
+
+	public Livro update(Integer id, Livro livroObj) {
+		Livro livroObjNew = metodoFindService(id);
+		atualizaLivro(livroObjNew, livroObj);
+		return livroRepositorio.save(livroObjNew);
+		
+	}
+
+	private void atualizaLivro(Livro livroObjNew, Livro livroObj) {
+		livroObjNew.setTexto(livroObj.getTexto());
+		livroObjNew.setTitulo(livroObj.getTitulo());
+	}
+
+	
 	
 
 	
